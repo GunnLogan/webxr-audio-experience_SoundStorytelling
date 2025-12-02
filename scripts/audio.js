be66419381a002e9fcecbeabcffb2913f3ad206d
opopolo <39377690+GunnLogan@users.noreply.github.com>
2025-12-02 16:54:25 +0100

    Refactor crossfade and handleIntroEnded functions

--- scripts/audio.js
+++ scripts/audio.js
@@ -1,24 +1,26 @@
-function crossfade(intro, amb, fadeTime=2000){
-  let start=null;
+function crossfade(intro, amb, fadeTime = 2000) {
+  let start = null;
 
-  function step(ts){
-    if(!start) start=ts;
-    let p=(ts-start)/fadeTime;
-    if(p>1) p=1;
+  function step(ts) {
+    if (!start) start = ts;
+    let p = (ts - start) / fadeTime;
+    if (p > 1) p = 1;
 
-    intro.setAttribute("sound","volume",1-p);
-    amb.setAttribute("sound","volume",p);
+    intro.setAttribute("sound", "volume", 1 - p);
+    amb.setAttribute("sound", "volume", p);
 
-    if(p<1) requestAnimationFrame(step);
+    if (p < 1) requestAnimationFrame(step);
     else intro.components.sound.stopSound();
   }
+
   requestAnimationFrame(step);
 }
 
-function handleIntroEnded(intro, ambient){
-  document.querySelectorAll("a-sphere[id^='a']").forEach(a=>{
-    a.setAttribute("visible","true");
-  });
+function handleIntroEnded(intro, ambient) {
+  // show A triggers
+  document.querySelectorAll("a-sphere[id^='a']").forEach((a) =>
+    a.setAttribute("visible", "true")
+  );
 
   ambient.components.sound.playSound();
   crossfade(intro, ambient);
